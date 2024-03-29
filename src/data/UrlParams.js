d857a82ecc75b0e06df5b39eeedf671de69738a8
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-03-29 16:26:48 -0600

    avance

---
+++ src/data/UrlParams.js
@@ -0,0 +1,3 @@
+const params = new URLSearchParams(window.location.search);
+const IdParams = params.get("id").toString();
+export const idP = IdParams;
---
+++ src/data/constantes.js
@@ -0,0 +1,4 @@
+import {idP} from "./constantes"
+const id = idP;
+export { id };
+
